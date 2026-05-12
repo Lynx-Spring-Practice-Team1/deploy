da5f9c102c96b95303e6397f8f64273cfadd15aa
Koncz Daniel <konczdaniel6@gmail.com>
2026-05-12 23:50:57 +0300

    broker-platform prototype

--- init/01_create_databases.sql
+++ init/01_create_databases.sql
@@ -1,7 +1,18 @@
 -- This script runs once on first PostgreSQL startup.
 -- auth_db is created automatically via POSTGRES_DB env var.
--- We create the remaining service databases here.
+-- postgres-init in docker-compose.yml repeats these idempotently for existing volumes.
 
-CREATE DATABASE orders_db;
-CREATE DATABASE wallet_db;
-CREATE DATABASE portfolio_db;
+SELECT 'CREATE DATABASE orders_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'orders_db')\gexec
+
+SELECT 'CREATE DATABASE wallet_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'wallet_db')\gexec
+
+SELECT 'CREATE DATABASE portfolio_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'portfolio_db')\gexec
+
+SELECT 'CREATE DATABASE market_notifications_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'market_notifications_db')\gexec
+
+SELECT 'CREATE DATABASE price_history_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'price_history_db')\gexec
